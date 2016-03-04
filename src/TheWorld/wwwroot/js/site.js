aa98da3f70ee0b85a52e5f09a9747b934326b98e
Pete Thompson <peter.thompson55@gmail.com>
2016-03-04 14:44:56 -0500

    Initial upload.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// site.js
+(function () {
+	
+	//var ele = $("#username");
+	//ele.text("Pete Thompson");
+
+	//var main = $("#main");
+	//main.on("mouseenter", function () {
+	//	main.style = "background-color: #888;";
+	//});
+
+	//main.on("mouseleave", function () {
+	//	main.style = "";
+	//});
+
+
+	//var menuItems = $("ul.menu li a");
+	//menuItems.on("click", function () {
+	//	var me = $(this);
+	//	alert(me.text());
+	//})
+
+	// wrapped set of DOM elements
+	var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+	$("#sidebarToggle").on("click", function () {
+		$sidebarAndWrapper.toggleClass("hide-sidebar");
+		if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+			$(this).text("Show Sidebar");
+		} else {
+			$(this).text("Hide Sidebar");
+		}
+	});
+})();
