a81538f25a832b16fa28b4b6bbfab6d79a1e90df
Pete Thompson <peter.thompson55@gmail.com>
2016-03-04 16:56:17 -0500

    Started Module 4, MVC 6.

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TheWorld.Controllers.Web
+{
+	public class AppController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		
+	}
+}
