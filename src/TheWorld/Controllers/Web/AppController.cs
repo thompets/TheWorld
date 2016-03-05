7654063ddc846605ca479d2722791851631abb8a
pete <peter.thompson55@gmail.com>
2016-03-04 21:39:33 -0500

    Finished Module 4

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNet.Mvc;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
 	public class AppController : Controller
 	{
+		private IMailService _mailService;
+
+		public AppController(IMailService service)
+		{
+			_mailService = service;
+		}
 		public IActionResult Index()
 		{
 			return View();
@@ -19,6 +27,29 @@ namespace TheWorld.Controllers.Web
 			return View();
 		}
 
-		
+		[HttpPost]
+		public IActionResult Contact(ContactViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+				if (string.IsNullOrWhiteSpace(email))
+				{
+					ModelState.AddModelError("", "Could not send email, configuration problem.");
+				}
+
+				if (_mailService.SendMail(email,
+					email, $"Contact Page from {model.Name} ({model.Email})",
+					model.Message))
+				{
+					ModelState.Clear();
+
+					ViewBag.Message = "Mail Sent.  Thanks!";
+				}
+			}
+			
+			return View();
+		}
 	}
 }
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,16 +1,37 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Services;
 
 namespace TheWorld
 {
 	public class Startup
 	{
+		public static IConfiguration Configuration;
+
+		public Startup(IApplicationEnvironment appEnv)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(appEnv.ApplicationBasePath)
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+
+			Configuration = builder.Build();
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc();
+
+#if DEBUG
+			services.AddScoped<IMailService, DebugMailService>();
+#else
+			services.AddScoped<IMailService, RealMailService>();
+#endif
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
