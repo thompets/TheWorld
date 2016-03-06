8f18b952f7538176067b54ff7744390b8ac2442c
pete <peter.thompson55@gmail.com>
2016-03-06 11:16:11 -0500

    Added TripController, deleting branch.

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+	public class TripController : Controller
+	{
+		private IWorldRepository _repository;
+
+		public TripController(IWorldRepository repository)
+		{
+			_repository = repository;
+		}
+
+		[HttpGet("api/trips")]
+		public JsonResult Get()
+		{
+			return Json(new { name = "Pete" });
+		}
+	}
+}
